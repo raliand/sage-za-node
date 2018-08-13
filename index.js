16c7f6c21ef18ad2bac1d647f5149953234f1461
Andrey Tchiorniy <andrey@healthbridge.co.za>
2018-08-13 15:34:13 +0200

    Fixed the post method

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var request = require('request');
-var utf8 = require('utf8');
 
-Sage.BASE_URL = "http://accounting.sageone.co.za/api/1.1.3";
+Sage.BASE_URL = "https://accounting.sageone.co.za/api/1.1.3";
 
 /* Constructor */
 function Sage(subscriptionKey, auth) {
@@ -10,13 +9,9 @@ function Sage(subscriptionKey, auth) {
 }
 
 /* Public Functions */
-Sage.prototype.query = function (httpMethod, url, parameters) {
+Sage.prototype.query = function (httpMethod, url, parameters, body) {
+
   url = Sage.BASE_URL + url;
-  let body = ''
-  if(httpMethod == 'POST' || httpMethod == 'PUT') {
-    body = utf8.encode(parameters.body)
-    delete parameters.body
-  }
 
   parameters.apikey = this.subscriptionKey
 
@@ -29,23 +24,17 @@ Sage.prototype.query = function (httpMethod, url, parameters) {
         'User-Agent': 'sage_za_node',
       },
     qs: parameters,
-    body: body
+    body: JSON.stringify(body)
   };
 
   return new Promise((resolve, reject) => {
-    // Making HTTP Request
     request(options, function (error, response, body) {
       if (response.statusCode == 200 || response.statusCode == 201) {
         resolve(JSON.parse(body));
       } else {
-        // console.log('response code:', response.statusCode);
-        // console.log(body);
         if (!error) error = { message: 'Error Occured', statusCode: response.statusCode }      
         reject(error)
       }
-      // console.log(response.request.uri)
-      // console.log(response.request.headers);
-      // console.log(response.request.body);
     });
   })
 };
